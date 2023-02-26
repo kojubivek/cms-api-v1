b5b086238144d183074e31620ece77ab589d0850
koju <kojubivek@gmail.com>
2023-02-27 09:23:20 +1100

    api

---
+++ server.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+//db connect
+import { dbConnect } from "./src/config/dbConfig.js";
+dbConnect();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// API routers
+import adminRouter from "./src/routers/adminRouter.js";
+import categoryRouter from "./src/routers/categoryRouter.js";
+
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/category", categoryRouter);
+
+//root url request
+app.use("/", (req, res, next) => {
+  const error = {
+    message: "You dont have promission here",
+  };
+  next(error);
+});
+
+//global error handler
+app.use((error, req, res, next) => {
+  console.log(error);
+  const statusCode = error.errorCode || 404;
+  res.status(statusCode).json({
+    status: "error",
+    message: error.message,
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server running at http://localhost:${PORT}`);
+});
